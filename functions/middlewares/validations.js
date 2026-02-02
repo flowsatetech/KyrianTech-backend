69063092f94deecdc41114d1d40a11ad7ef277ab
Daniella <onasogaemmanuel02@gmail.com>
2026-02-02 04:36:57 +0100

    Fixed validation issues

--- functions/middlewares/validations.js
+++ functions/middlewares/validations.js
@@ -9,16 +9,16 @@ const { body } = require('express-validator');
 
 
 const signupValidation = [
-    body('firstName').trim().isLength({ min: 1 }).withMessage('First Name must be at least 1 characters'),
-    body('lastName').trim().isLength({ min: 1 }).withMessage('Last Name must be at least 1 characters'),
-    body('email').isEmail().normalizeEmail().withMessage('Invalid email address'),
+    body('firstName').isString().trim().isLength({ min: 1 }).withMessage('First Name must be at least 1 characters'),
+    body('lastName').isString().trim().isLength({ min: 1 }).withMessage('Last Name must be at least 1 characters'),
+    body('email').isEmail().trim().toLowerCase().withMessage('Invalid email address'),
     body('phone').trim().isMobilePhone('any').withMessage('Invalid phone number'), // en-NG
     body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/).withMessage('Password must contain uppercase, lowercase, and number')
 ];
 
 const signinValidation = [
-    body('email').isEmail().normalizeEmail().withMessage('Invalid email address'),
+    body('email').isEmail().trim().toLowerCase().withMessage('Invalid email address'),
     body('password').notEmpty().withMessage('Password is required')
 ];
 
