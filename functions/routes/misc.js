164d68f1dd9a67f91a9c13c49dd9b9827ccd48aa
Daniella <onasogaemmanuel02@gmail.com>
2026-02-11 12:11:02 +0100

    Redis flush route added (Admin only)

--- functions/routes/misc.js
+++ functions/routes/misc.js
@@ -6,6 +6,9 @@
 const express = require('express');
 
 // <-- LOCAL EXPORTS IMPORTS -->
+const { adminOnly, authMiddleware } = require('../middlewares');
+const redis = require('../middlewares/utils/redis_client');
+const { logger } = require('../helpers');
 
 /** SETUP
  * Global variables referenced in this file are defined here
@@ -20,6 +23,26 @@ router.get('/health', (req, res) => {
     timestamp: Date.now()
   });
 });
+
+router.delete('/redis/flush', authMiddleware, adminOnly, async (req, res) => {
+  try {
+    await redis.flushAll();
+
+    return res.status(200).json({
+      success: true,
+      message: 'Redis cache flushed successfully'
+    });
+
+  } catch (err) {
+    logger('REDIS_FLUSH').error(err);
+
+    return res.status(500).json({
+      success: false,
+      message: 'Failed to flush Redis'
+    });
+  }
+});
+
 
 /** EXPORTS
  * Export Routes for use in routers
